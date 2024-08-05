226fefeb01b7bc08eee80c76fb257f32b74146d9
Sai kumar <84771018+Saikumar77-Ch@users.noreply.github.com>
2024-08-05 18:36:49 +0530

    Add files via upload

---
+++ Sorting & Searching/Maximum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class Maximum
+{
+   public static void main(String[] args) throws IOException
+   {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int a = Integer.parseInt(st.nextToken());
+       st=new StringTokenizer(br.readLine());
+       long sum=0;
+       long max=Long.MIN_VALUE;
+       while(a-->0)
+       {
+          long b=Integer.parseInt(st.nextToken());
+          sum+=b;
+          max=Math.max(sum,max);
+          if(sum<0) sum=0;
+       }
+       System.out.println(max);
+   }
+}
+          
+          
